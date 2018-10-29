b76b1c8dc8394b025dbb3f024830ccd9f47c2936
Yohann Meyer <yohann.meyer@gmail.com>
2018-10-29 10:56:45 +0100

    chmod

--- run.sh
+++ run.sh
@@ -1,7 +1,9 @@
+chmod -R a+w site/database-R s
 cd site/databases
 sqlite3 database.sqlite < database_users_base_demo.sql
 sqlite3 database.sqlite < database_messages_base_demo.sql
 cd ../..
+
 docker stop sti_project
 docker rm sti_project
 docker run -ti -v /home/nyahon/Documents/HEIG/HEIG-S5/STI/projet/site:/usr/share/nginx/ -d -p 8080:80 --name sti_project --hostname sti arubinst/sti:project2018
